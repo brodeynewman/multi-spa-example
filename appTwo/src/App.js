60a35c8c26266c8d7fba165fbc5bdb3b670c0438
Brodey Newman <brodeynewman@gmail.com>
2018-09-09 14:28:05 -0400

    added styling

--- appTwo/src/App.js
+++ appTwo/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Sidebar from './Sidebar';
+
+const styles = {
+  container: {
+    display: 'flex',
+  },
+  content: {
+    padding: 25,
+  },
+};
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello there from app two
+      <div style={styles.container}>
+        <Sidebar />
+        <div style={styles.content}>
+          Hello, this is App Two being rendered from a different s3 bucket.
+        </div>
       </div>
     )
   }
---
+++ appTwo/src/Sidebar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const linkStyles = {
+  color: '#ddd',
+  padding: '25px 15px 25px 25px',
+  display: 'block',
+  textDecoration: 'none',
+};
+
+const styles = {
+  nav: {
+    width: 225,
+    height: '100vh',
+    backgroundColor: '#1F2141',
+  },
+  link: {
+    ...linkStyles,
+    backgroundColor: 'rgba(255, 255, 255, .1)',
+  },
+  inactiveLink: {
+    ...linkStyles,
+  },
+  logo: {
+    color: '#fff',
+    fontSize: 25,
+    textAlign: 'center',
+    marginBottom: 15,
+    marginTop: 25,
+  },
+};
+
+const Sidebar = () => (
+  <div style={styles.nav}>
+    <div style={styles.logo}>Platform</div>
+    <a style={styles.inactiveLink} href="/foo/bar">Go to app one</a>
+    <a style={styles.link} href="/foo/baz">Go to app two</a>
+  </div>
+);
+
+export default Sidebar;
